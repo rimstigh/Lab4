917a522b65f6331439f8fc16463ec8caeab0898a
rimstigh <rimstigh@mail.uc.edu>
2020-09-29 18:24:07 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,32 @@
+/*Lab 4
+Partners Grant Rimsdt and Sam Hunkler
+9/16/2020
+*/
+import java.util.*;
+class Main {
+
+  public static void main(String[] args) {
+    Random r =new Random();
+    int randomNum = r.nextInt(51);
+    System.out.println("The random number is " + randomNum);
+    countDown(randomNum);
+    if (randomNum < 25)
+    System.out.println("Ahoy Matey!");
+    if (42>randomNum && randomNum >25)
+    System.out.println("Cannonball!");
+    if (randomNum > 42)
+    System.out.println("Blastoff!");
+  }
+
+
+
+static int countDown(int rand) {
+  if (rand > 1)
+    do {
+    rand--;
+   System.out.println(rand);
+    }  while (rand > 0);
+  return rand;
+}
+  
+}
